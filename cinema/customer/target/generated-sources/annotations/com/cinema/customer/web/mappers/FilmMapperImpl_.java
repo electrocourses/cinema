e2041864ab41b2986d6cf89aa1fde65e82cc9d24
Dima <salomakhin.dmytro@gmail.com>
2023-01-20 01:04:43 +0200

    Added JARs to docker images

--- cinema/customer/target/generated-sources/annotations/com/cinema/customer/web/mappers/FilmMapperImpl_.java
+++ cinema/customer/target/generated-sources/annotations/com/cinema/customer/web/mappers/FilmMapperImpl_.java
@@ -24,8 +24,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-19T20:04:05+0200",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
+    date = "2023-01-20T00:55:11+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.4.1 (Oracle Corporation)"
 )
 @Component
 @Qualifier("delegate")
--- cinema/notification/target/generated-sources/annotations/com/cinema/notification/web/mappers/NotificationMapperImpl.java
+++ cinema/notification/target/generated-sources/annotations/com/cinema/notification/web/mappers/NotificationMapperImpl.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-19T18:43:51+0200",
-    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.1 (Oracle Corporation)"
+    date = "2023-01-20T00:56:07+0200",
+    comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.4.1 (Oracle Corporation)"
 )
 @Component
 @Primary
