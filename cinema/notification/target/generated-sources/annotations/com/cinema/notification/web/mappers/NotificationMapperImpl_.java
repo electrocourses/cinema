90a7819268bebd041ecd9f91c2b03074aad20875
Dima <salomakhin.dmytro@gmail.com>
2023-01-20 15:24:33 +0200

    Refactored microservices

--- cinema/notification/target/generated-sources/annotations/com/cinema/notification/web/mappers/NotificationMapperImpl_.java
+++ cinema/notification/target/generated-sources/annotations/com/cinema/notification/web/mappers/NotificationMapperImpl_.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-20T00:56:07+0200",
+    date = "2023-01-20T15:18:14+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.4.1 (Oracle Corporation)"
 )
 @Component
